92c164fddcc03b51edcfd1a70ad1ffd74a544cf4
Alexander Nohe <alex.nohe427@gmail.com>
2015-11-05 16:27:32 -0500

    First Commit

---
+++ ArcMonsters-monsters/src/Employees.java
@@ -0,0 +1,71 @@
+/**
+ * Created by alex7370 on 11/5/2015.
+ */
+public class Employees {
+    private int level;
+    private long currentXP;
+    private long nextXP;
+    public enum TeamType {SDK, DESKTOP, SERVER, IMPLEMENTATION, GEODATA};
+    private TeamType EmpType;
+    private String name;
+
+    Employees(String Name, long xp, TeamType TypeOfCharacter)
+    {
+        this.name = Name;
+        this.EmpType = TypeOfCharacter;
+        this.currentXP = 0;
+        this.level = 1;
+        this.nextXP = 222;
+        this.adjustXP(xp);
+    }
+
+    Employees()
+    {
+        new Employees("David", 25, TeamType.SDK);
+    }
+
+    Employees(String Name)
+    {
+        new Employees(Name, 25, TeamType.DESKTOP);
+    }
+
+    Employees(String Name, long xp)
+    {
+        new Employees(Name, xp, TeamType.DESKTOP);
+    }
+
+    Employees(String Name, TeamType TypeOfCharacter)
+    {
+        new Employees(Name, 25, TypeOfCharacter);
+    }
+
+    public int getLevel()
+    {
+        return this.level;
+    }
+
+    public long getCurrentXP()
+    {
+        return this.currentXP;
+    }
+
+    public void adjustXP(long xp)
+    {
+        if ((this.currentXP + xp) > this.nextXP)
+        {
+            this.level++;
+            this.nextXP += 222*this.level;
+            this.currentXP = this.nextXP;
+            xp -= this.nextXP;
+            adjustXP(xp);
+        }
+        else {
+            this.currentXP+=xp;
+        }
+    }
+
+    public TeamType getType()
+    {
+        return this.EmpType;
+    }
+}
