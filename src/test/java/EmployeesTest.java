56144b2efc2f029e2c3234563e0d177e976dddde
Alexander Nohe <alex.nohe427@gmail.com>
2015-11-06 14:53:28 -0500

    :+1: adjusted some tests to account for constructors.

--- src/test/java/EmployeesTest.java
+++ src/test/java/EmployeesTest.java
@@ -37,4 +37,24 @@ public class EmployeesTest {
     public void testGetType() throws Exception {
         assertEquals(Employees.TeamType.SDK, employee.getType());
     }
+
+    @org.junit.Test
+    public void testGetName() throws Exception {
+        assertEquals("David", employee.getName());
+    }
+
+    @org.junit.Test
+    public void testConstructors() throws Exception {
+        Employees emp1 = new Employees("Kwasi");
+        assertEquals("Kwasi", emp1.getName());
+        assertEquals(Employees.TeamType.SDK,emp1.getType());
+        assertEquals(1, emp1.getLevel());
+        assertEquals(25, emp1.getCurrentXP());
+
+        Employees emp2 = new Employees("Kwasi",937);
+        assertEquals("Kwasi", emp2.getName());
+        assertEquals(937, emp2.getCurrentXP());
+        assertEquals(3, emp2.getLevel());
+        assertEquals(Employees.TeamType.SDK,emp2.getType());
+    }
 }
