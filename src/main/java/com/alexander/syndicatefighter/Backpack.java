ebd891a3dff879ab123b29c9caa641686749fe33
Alexander Nohe <nohe427@users.noreply.github.com>
2015-11-23 15:54:57 -0500

    Needs polishing.

--- src/main/java/com/alexander/syndicatefighter/Backpack.java
+++ src/main/java/com/alexander/syndicatefighter/Backpack.java
@@ -1,28 +1,37 @@
 package com.alexander.syndicatefighter;
 
-import java.util.List;
+import java.util.*;
 
 /**
- * Created by mohi7253 on 11/18/2015.
- */
+* Created by mohi7253 on 11/18/2015.
+*/
 public class Backpack {
-    private List<Item> items;
-    private List<Integer> itemCount;
+    //private List<Item> items;
+    //private List<Integer> itemCount;
+
+    private String itemName;
+    private int itemCount;
+    private Map<Item, Integer> items = new HashMap<Item, Integer>();
+
+    // Associative Arrays:
 
     public Backpack() {
 
     }
 
-    public void useItem(int indexOfItems) throws Exception {
-
+    public void useItem(Item item) throws Exception {
+        items.remove(item);
+        //decrease count by 1
     }
 
-    public int addItem(Item item) throws Exception {
-        return items.indexOf(item);
+    public void addItem(Item item) throws Exception {
+        items.put(item, itemCount + 1);
     }
 
-    public void recycleItem(int indexOfItems) throws Exception {
-
+    public void recycleItem(Item item) throws Exception {
+        items.remove(item);
+        //decrease itemCount by 1
+        //increase power
     }
 
    /* public int searchItem(Item item) throws Exception {
@@ -30,7 +39,6 @@ public class Backpack {
     }*/
 
     public Item listItem(Item item) throws Exception {
-
             //return this.items, this.count to peek in the backpack
     }
 
