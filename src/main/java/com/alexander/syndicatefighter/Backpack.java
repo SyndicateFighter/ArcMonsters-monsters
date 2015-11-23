841fe3ac346fbc8945b6fed5cee6df8cfe8c6fc1
Alexander Nohe <alex.nohe427@gmail.com>
2015-11-23 16:09:43 -0500

    Updated to resolve a few issues.  Will write tests later

--- src/main/java/com/alexander/syndicatefighter/Backpack.java
+++ src/main/java/com/alexander/syndicatefighter/Backpack.java
@@ -1,45 +1,45 @@
 package com.alexander.syndicatefighter;
 
+import java.net.Inet4Address;
 import java.util.*;
 
 /**
 * Created by mohi7253 on 11/18/2015.
 */
 public class Backpack {
-    //private List<Item> items;
-    //private List<Integer> itemCount;
 
-    private String itemName;
-    private int itemCount;
     private Map<Item, Integer> items = new HashMap<Item, Integer>();
-
-    // Associative Arrays:
 
     public Backpack() {
 
     }
 
     public void useItem(Item item) throws Exception {
-        items.remove(item);
-        //decrease count by 1
+        int countOfItem = items.get(item);
+        if (countOfItem == 1) {
+            items.remove(item);
+        } else {
+            items.put(item, (countOfItem -1));
+        }
     }
 
     public void addItem(Item item) throws Exception {
-        items.put(item, itemCount + 1);
+        int countOfItem = items.get(item);
+        items.put(item, countOfItem + 1);
     }
 
     public void recycleItem(Item item) throws Exception {
         items.remove(item);
-        //decrease itemCount by 1
-        //increase power
     }
 
-   /* public int searchItem(Item item) throws Exception {
-        return item.index;
-    }*/
+    public List<String> listItem(Item item) throws Exception {
 
-    public Item listItem(Item item) throws Exception {
-            //return this.items, this.count to peek in the backpack
+        List<String> allItems = new ArrayList<String>();
+
+        for (Map.Entry<Item, Integer> entry : this.items.entrySet()) {
+            allItems.add("Item: " + entry.getKey().getName() + ", Count: " + entry.getValue().toString());
+        }
+        return allItems;
     }
 
     //Methods to implement later: storeToJSON() : void
